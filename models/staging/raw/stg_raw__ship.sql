340d271646d264ad3f55d7605ef4199246469d5f
el.moutassim.mery@gmail.com <el.moutassim.mery@gmail.com>
2025-02-08 14:44:58 +0000

    generated models and data cleaning

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        cast(ship_cost as integer)
+
+    from source
+
+)
+
+select * from renamed
